9b65b693022a8ebc0f0fa77840176db796d37227
CiprianAninoiu-Yields <ciprian.aninoiu@yields.io>
2021-09-22 22:23:41 +0300

    test1

--- PlaceholdersAddInWeb/Sections/ViewPlaceholderDialog/ViewPlaceholder.js
+++ PlaceholdersAddInWeb/Sections/ViewPlaceholderDialog/ViewPlaceholder.js
@@ -13,10 +13,10 @@
         }
     }
     Office.initialize = function (reason) {
-        placeholderTag = GetURLParameter('tag').replace(/%3E/g, " ").replace(/%3/g, " ").replace(/%20/g, " ");
-        description = GetURLParameter('description').replace(/%3E/g, " ").replace(/%3/g, " ").replace(/%20/g, " ");
+        placeholderTag = GetURLParameter('tag');
+        description = GetURLParameter('description');
         var holder = document.getElementById("holder");
-        holder.innerHTML += '<h3 style="text-align: center;">' + placeholderTag + '</h3>';
+        holder.innerHTML += '<h3 style="text-align: center;">' + placeholderTag + '</h3><hr>';
         holder.innerHTML += '<p style="text-align: center;">Tag name is ' + placeholderTag + '.</p>';
         holder.innerHTML += '<p style="text-align: center;">' + description + '</p>';
     };
