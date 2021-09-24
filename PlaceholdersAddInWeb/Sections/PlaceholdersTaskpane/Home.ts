3ebe299f472a6647a6ad147817b60b5c62ac45dd
CiprianAninoiu-Yields <ciprian.aninoiu@yields.io>
2021-09-24 11:46:30 +0300

    details dialog

--- PlaceholdersAddInWeb/Sections/PlaceholdersTaskpane/Home.ts
+++ PlaceholdersAddInWeb/Sections/PlaceholdersTaskpane/Home.ts
@@ -161,7 +161,7 @@ function openSettings(event) {
 }
 
 function openDetails(event) {
-    Office.context.ui.displayDialogAsync('https://cipriananinoiu-yields.github.io/docbuilderaddin.github.io/PlaceholdersAddInWeb/Sections/SettingsDialog/Settings.html', { height: 45, width: 45 },
+    Office.context.ui.displayDialogAsync('https://cipriananinoiu-yields.github.io/docbuilderaddin.github.io/PlaceholdersAddInWeb/Sections/DetailsDialog/Details.html', { height: 45, width: 45 },
         function (asyncResult) {
             dialog = asyncResult.value;
             dialog.addEventHandler(Office.EventType.DialogMessageReceived, processSettings);
